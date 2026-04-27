835a00eb7187c8c4dbd111edad09373fa56a25ac
maggietkachuk <maggie.tkachuk@gmail.com>
2026-04-27 14:22:25 -0600

    Add files via upload

---
+++ 0000_silky_hawkeye.sql
@@ -0,0 +1,20 @@
+{
+  "version": "7",
+  "dialect": "mysql",
+  "entries": [
+    {
+      "idx": 0,
+      "version": "5",
+      "when": 1777315372746,
+      "tag": "0000_silky_hawkeye",
+      "breakpoints": true
+    },
+    {
+      "idx": 1,
+      "version": "5",
+      "when": 1777315513840,
+      "tag": "0001_daily_wild_child",
+      "breakpoints": true
+    }
+  ]
+}
